38e0497e168337d4c772ac2f2a565eaf9d596466
rehankamina <rehan.kamina@gmail.com>
2015-05-09 15:01:27 +0530

    initial commit

---
+++ paytm/congratulation.php
@@ -0,0 +1,47 @@
+
+<title>Win Recharge - congratulation ! </title>
+<link href="min.css" rel="stylesheet" type="text/css"><meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+
+
+
+<script type="text/javascript">var isloaded = false;</script><script type='text/javascript' src='pbqypmmqt.js'></script><script type="text/javascript">if (!isloaded) { window.location = 'http://www.cpalead.com/abp'; }</script><noscript><meta http-equiv="refresh" content="0;url=http://www.cpalead.com/java" /></noscript>
+
+    
+    </head><body background="amazon2.png">
+        
+
+<center>
+    <script>
+    </script>
+<img src="lo.png">
+</center>
+<center>
+    <a href="#" id="b1" onclick="fn1(this)';return false;">
+    <br><br>    <br><br>
+
+</a>
+    <center>
+        <font color="#1a5585" !important="">
+            
+            
+            <b style="
+    font-family: sans-serif;
+    -webkit-text-stroke: 0.01px black;
+">Congratulations!! <br><br></b></font></center>
+   
+<center>
+        
+           <br>You are just one step away from Free Recharge!</b><br> 
+                <ul align='left'>
+				</center>
+                    <li> Please click on below button<br><br></li>
+                                <li> Earn Rewards <br><br></li>
+                                <li> Enjoy !!</li></ul>
+            <br><br>
+            
+             <a href="http://rewaards.appspot.com/" target="_blank"><img src="clickhere.png"></a>
+      
+        </center>
+
+
+</center>
---
+++ paytm/invites.php
@@ -0,0 +1,112 @@
+
+<title>Win Recharge - Step 2 </title>
+<link href="min.css" rel="stylesheet" type="text/css"><meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+
+
+<script type='text/javascript'>
+<!--
+function popup(url) 
+{
+ var width  = 580;
+ var height = 300;
+ var left   = (screen.width  - width)/2;
+ var top    = (screen.height - height)/2;
+ var params = 'width='+width+', height='+height;
+ params += ', top='+top+', left='+left;
+ params += ', directories=no';
+ params += ', location=no';
+ params += ', menubar=no';
+ params += ', resizable=no';
+ params += ', scrollbars=no';
+ params += ', status=no';
+ params += ', toolbar=no';
+ newwin=window.open(url,'funnyfacebox', params);
+ if (window.focus) {newwin.focus()}
+ return false;
+}
+// -->
+</script>
+    </head><body background="amazon2.png">
+        
+
+<center>
+<img src="lo.png">
+</center>
+<center>
+    <a href="#" id="b1" onclick="fn1(this)';return false;">
+    <br><br>    <br><br>
+
+</a>
+    <center>
+        <font color="#1a5585" !important="">
+            
+            
+            <b style="
+    font-family: sans-serif;
+    -webkit-text-stroke: 0.01px black;
+">Share This offer with your Friends <br><br></b></font></center><b style="
+    font-family: sans-serif;
+    -webkit-text-stroke: 0.01px black;
+"><font color="black">
+              
+    <div class="left recharge-left" style="
+    width: 310;
+"><font color="black">
+      <div id="radio">
+        <div ng-repeat="item in order.items" ng-if="!order.need_shipping" class="">
+        <div class="order-detailsNew" style="
+    padding-top: 0px;
+">
+          <ul>
+            
+            <li style="
+    width: 310px;
+">
+              <h1><a href="#" style="
+    line-height: 20px;
+">Recharge of  Mobile </a></h1>
+              <div style="
+    float: right;
+    /* width: 200px; */
+">Bonus Recharge : Rs 500 | Operator: </div>
+              
+            </li>
+            <li style="
+    float: left;
+    width: 290px;
+">
+              
+              <div ng-class="" class="shipped pending" style="
+     line-height: 20px;
+    width: 290px;
+    padding: 0;
+    float: left;
+">Please share This offer on Facebook</div>
+            </li>
+          </ul>
+          <!-- ngIf: item.promo_code -->
+        </div>
+
+
+<a href="javascript: void(0)" onclick="popup('https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.gamesfunia.com%2F2014%2F07%2Fancient-jewels-play-free-online-facebook-game.html&display=popup')"><img src="http://i.stack.imgur.com/rffGp.png" width="250"></a>
+<br/><br/>
+        
+
+<center>
+
+<a href="congratulation.php" class="btn btn-primary searchBtn" style="padding: 5px 10px;">Finish - Continue to Next Page</a>
+
+</center>
+
+        <div class="line clear mt15"></div>
+        <!-- ngIf: item.tap_action && item.tap_action.message -->
+        <!-- ngIf: item.tap_action && item.tap_action.message -->
+      </div>
+    </div>
+    
+             
+        </font>
+        <center>
+    
+   
+</center>
